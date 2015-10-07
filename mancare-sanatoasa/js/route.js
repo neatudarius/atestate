63b1a8e410004cc9603281d9c9a132b5bdfe8a66
neatudarius <neatudarius@yahoo.com>
2015-10-07 16:10:55 +0300

    Add pages on mancare-sanatoasa

---
+++ mancare-sanatoasa/js/route.js
@@ -0,0 +1,37 @@
+farmApp.config( function( $routeProvider, $locationProvider ) {
+
+    $routeProvider.when('/', {
+        templateUrl: 'partials/acasa.html'
+    });
+
+    $routeProvider.when('/acasa', {
+        templateUrl: 'partials/acasa.html'
+    });
+
+    $routeProvider.when('/produse', {
+        templateUrl: 'partials/produse.html'
+    });
+	
+    $routeProvider.when('/contact', {
+        templateUrl: 'partials/contact.html'
+    });
+
+    $routeProvider.when('/despre', {
+        templateUrl: 'partials/despre.html'
+    });
+
+
+    $routeProvider.when('/specialitatea-casei', {
+        templateUrl: 'partials/specialitatea-casei.html'
+    });
+
+    $routeProvider.when('/planuri-de-viitor', {
+        templateUrl: 'partials/planuri-de-viitor.html'
+    });
+
+    $routeProvider .otherwise({
+        redirectTo: '/404'
+    });
+
+
+});
